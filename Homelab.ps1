edf67f6f90ee8a5caf8e11435369a6029a6ae212
ModQuad <68094306+ModQuadWarren@users.noreply.github.com>
2024-02-05 18:49:55 -0600

    first

---
+++ Homelab.ps1
@@ -0,0 +1,14 @@
+# check current WSMan Trusted Hosts:
+Get-Item WSMan:\localhost\Client\TrustedHosts
+
+# add to TrustedHosts after enabling PSRemoting
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "dc01,dc02,dc03,mssql01,srv01"
+
+# check current WSMan delegates:
+(Get-Item WSMan:\localhost\Client\TrustedHosts).Value
+
+# add delegate computer
+Enable-WSManCredSSP -Role client -DelegateComputer dc03
+
+# add cmdkey
+cmdkey /add:dc03 /user:caseylab\rob /pass

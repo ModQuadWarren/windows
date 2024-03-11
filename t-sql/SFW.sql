fa76530c243ffde056ba01c96681b6648e8f15fd
Rob Casey <rob.casey@InfiniteCampus.com>
2024-03-11 11:26:43 -0500

    Signed-off-by: Rob Casey <rob.casey@InfiniteCampus.com>

--- t-sql/SFW.sql
+++ t-sql/SFW.sql
@@ -61,5 +61,13 @@ FROM Production.Product
 WHERE ListPrice > 0;
 
 -- Challenge: Write a SELECT Statement
-SELECT  
-WHERE
+SELECT WorkOrderID AS 'Work Order ID',
+	ScrappedQty AS 'Scrapped Quantity',
+	StartDate AS 'Start Date',
+	EndDate AS 'End Date'
+FROM Production.WorkOrder
+WHERE ScrappedQty > 0 
+	AND StartDate >= '2013-12-01'
+	AND EndDate <= '2013-12-31'
+ORDER BY ScrappedQty DESC
+;

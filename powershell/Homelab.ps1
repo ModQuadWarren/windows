70141c133a65be069d923c3a555b41c5755b0428
ModQuad <68094306+ModQuadWarren@users.noreply.github.com>
2024-05-13 18:41:38 -0500

    msg# Please enter the commit message for your changes. Lines starting

--- powershell/Homelab.ps1
+++ powershell/Homelab.ps1
@@ -3,13 +3,19 @@
 Get-Item WSMan:\localhost\Client\TrustedHosts
 
 # add to TrustedHosts after enabling PSRemoting
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value "dc01,dc02,dc03,mssql01,srv01"
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "dc01,dc02,mssql01,mstomcat"
+
+# remove from TrustedHosts
+Remove-Item WSMan:\localhost\Client\TrustedHosts -Value "dc01,dc02,mssql01,mstomcat"
 
 # check current WSMan delegates:
 (Get-Item WSMan:\localhost\Client\TrustedHosts).Value
 
 # add delegate computer
-Enable-WSManCredSSP -Role client -DelegateComputer dc03
+Enable-WSManCredSSP -Role client -DelegateComputer dc01
 
-# add cmdkey
-cmdkey /add:dc03 /user:caseylab\rob /pass
+# add cmdkey (local)
+cmdkey /add:dc02 /user:dc02\Administrator /pass
+
+# add cmdkey (domain)
+cmdkey /add:dc01 /user:caseylab\rob /pass
